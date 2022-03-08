61314c8888bc067c44642ada5f7857ffa831a9a0
Bryce Payne <bryce.payne.97@gmail.com>
2022-03-08 11:33:51 -0600

    line 5

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 // simple hello world node app 
 const APP_PORT = process.env.APP_PORT || 3000; app.get('/', (req, res) => {
-    res.send('hello bryce! nice to meet you!')
+    res.send('hello bryce! nice to see you!')
 }); app.listen(APP_PORT, () => {
     console.log(`Running app at port:${APP_PORT}`)
 })
