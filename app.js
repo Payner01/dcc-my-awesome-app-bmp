e02261815165722ba3800ebe0b6d4c145e5ca416
Bryce Payne <90872146+Payner01@users.noreply.github.com>
2022-03-08 10:51:38 -0600

    Update app.js
    changed sentence

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 // simple hello world node app 
 const APP_PORT = process.env.APP_PORT || 3000; app.get('/', (req, res) => {
-    res.send('hello world')
+    res.send('hello to bryce and the rest of the world out there!')
 }); app.listen(APP_PORT, () => {
     console.log(`Running app at port:${APP_PORT}`)
 })
